4a0ae6e19218ba333e40309365d5c63ecb6ea424
otterHooligan <kristy.majetich@yahoo.com>
2022-12-18 23:15:50 -0500

    get last name function

--- src/Ump/flavor_text_generator.py
+++ src/Ump/flavor_text_generator.py
@@ -12,10 +12,18 @@ def get_current_lineup(league, season, session, game_id, home):
     sql = '''SELECT player_id from lineups WHERE league=%s AND season=%s AND session=%s AND game_id=%s AND home=%s AND position=%s ORDER BY row_id DESC'''
     for position in positions:
         p = db.fetch_data(sql, (league, season, session, game_id, home, position))
-        if len(p) > 1:
-            p = p[0]
-        else:
+        if len(p) >= 1:
             p = p[0]
         current_positions[position] = db.fetch_one('''SELECT playerName FROM playerData WHERE playerID=%s''', p)[0]
-    print(current_positions)
     return current_positions
+
+
+def get_last_name(name):
+    not_last_names = ['jr', 'ii', 'iii', 'iv', 'vi',  'xiv', 'cdxx', 'cccxxvii', 'lxix', 'esq', 'sr']
+    name = name.split(' ')
+    last_name = name[-1]
+    if last_name.replace('.', '').lower() in not_last_names:
+        new_name = name[:-1]
+        last_name = get_last_name(' '.join(new_name))
+    return last_name
+
